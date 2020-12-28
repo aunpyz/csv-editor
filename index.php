ce915fb9297eb8ca983edbf1cbfa41cd934674f7
Aunpyz <aunnop_kattiyanet@hotmail.com>
2020-12-28 22:51:36 +0700

    Show file name, unserialize serialized data & prevent errors

--- index.php
+++ index.php
@@ -1,33 +1,84 @@
 <form action="index.php" method="POST" enctype="multipart/form-data">
-    <input id="file" name="file" type="file" accept=".csv">
-    <input type="submit" value="Submit" onclick="() => e.preventDefault">
+    <input id="file" name="file" type="file" accept=".csv" onchange="resetLoadFileButton()">
+    <input id="file-open" type="submit" value="Open file">
 </form>
 
 <?php
     if (isset($_FILES['file'])) {
+        $serialized_pattern = "/^a:\d+:{/";
         $file_path = $_FILES['file']['tmp_name'];
-        $file = fopen($file_path, "r+");
-        $fields = fgetcsv($file);
-        $id_idx = array_search("id", $fields, true);
+        try {
+            $file = fopen($file_path, "r+");
+            $fields = fgetcsv($file);
+            $id_idx = array_search("id", $fields, true);
 
-        while(!feof($file)) {
-            $record = fgetcsv($file);
-            echo "<div class='csv-data' data-id={$record[$id_idx]}>";
-            for($i = 0; $i < count($record); ++$i) {
-                echo $fields[$i] . " : ";
-                echo "<input type='text' value={$record[$i]}>";
-                echo "<br/>";
+            echo "<label for='filename'>File name</label>";
+            echo "<input id='filename' value='{$_FILES['file']['name']}'>";
+            while(!feof($file)) {
+                $record = fgetcsv($file);
+                // empty line
+                if (empty($record)) {
+                    break;
+                }
+                echo "<div class='csv-data' data-id='{$record[$id_idx]}'>";
+                for($i = 0; $i < count($record); ++$i) {
+                    $current = $record[$i];
+                    echo "<strong>$fields[$i]: </strong>";
+                    if (preg_match($serialized_pattern, $current)) {
+                        $data = unserialize($current);
+                        echo "<div class='unserialized' data-serialize='$fields[$i]'>";
+                        foreach ($data as $key => $value) {
+                            echo "<label>Key: </label>";
+                            echo "<span data-key='key' role='textbox' contenteditable>{$key}</span>";
+                            echo "<label>Value: </label>";
+                            echo "<span data-key='value' role='textbox' contenteditable>{$value}</span>";
+                            echo "<br/>";
+                        }
+                        echo "</div>";
+                    } else {
+                        echo "<span role='textbox' contenteditable>{$current}</span>";
+                        echo "<br/>";
+                    }
+                }
+                echo "</div>";
             }
-            echo "</div>";
+            fclose($file);
+        } catch (Exception $e) {
+            echo "<div class='error'>{$e->getMessage()}</div>";
         }
-        fclose($file);
     }
 ?>
 
+<script>
+    const resetLoadFileButton = () => loadFileButton.disabled = !file.value;
+
+    const loadFileButton = document.getElementById("file-open");
+    const file = document.getElementById("file");
+
+    resetLoadFileButton();
+</script>
+
 <style>
+    html {
+        line-height: 1.5rem;
+    }
+    span[role="textbox"] {
+        display: inline-block;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        padding: 1px 6px;
+        min-width: 1ch;
+        margin: 0 8px;
+    }
     .csv-data {
         border: 2px solid red;
         margin: 8px;
         padding: 8px;
+    }
+    .csv-data > .unserialized {
+        margin-left: 8px;
+    }
+    .error {
+        color: red;
     }
 </style>
