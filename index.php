913e2d64e2b86f90d3403bc0bba6397e4ff24026
Aunpyz <aunnop_kattiyanet@hotmail.com>
2021-01-01 02:47:58 +0700

    Get list of unmodified data in POST

--- index.php
+++ index.php
@@ -1,60 +1,70 @@
 <form action="index.php" method="POST" enctype="multipart/form-data">
     <input id="file" name="file" type="file" accept=".csv" onchange="resetLoadFileButton()">
-    <input id="file-open" type="submit" value="Open file">
+    <button id="file-open" type="submit" value="open">Open file</button>
 </form>
 
-<?php
-    if (isset($_FILES['file'])) {
-        $serialized_pattern = "/^a:\d+:{/";
-        $file_path = $_FILES['file']['tmp_name'];
-        try {
-            $file = fopen($file_path, "r+");
-            $fields = fgetcsv($file);
-            $id_idx = array_search("id", $fields, true);
+<form action="savefile.php" method="POST" enctype="multipart/form-data">
+    <?php
+        if (isset($_FILES['file'])) {
+            $serialized_pattern = "/^a:\d+:{/";
+            $file_path = $_FILES['file']['tmp_name'];
+            try {
+                $file = fopen($file_path, "r+");
+                $fields = fgetcsv($file);
+                $id_idx = array_search("id", $fields, true);
 
-            echo "<label for='filename'>File name</label>";
-            echo "<input id='filename' value='{$_FILES['file']['name']}'>";
-            while(!feof($file)) {
-                $record = fgetcsv($file);
-                // empty line
-                if (empty($record)) {
-                    break;
-                }
-                echo "<div class='csv-data' data-id='{$record[$id_idx]}'>";
-                for($i = 0; $i < count($record); ++$i) {
-                    $current = $record[$i];
-                    echo "<strong>$fields[$i]: </strong>";
-                    if (preg_match($serialized_pattern, $current)) {
-                        $data = unserialize($current);
-                        echo "<div class='unserialized' data-serialize='$fields[$i]'>";
-                        foreach ($data as $key => $value) {
-                            echo "<section>";
-                            echo "<label>Key: </label>";
-                            echo "<input data-key='key' onkeyup='validateUnserializedFields(event)' value='{$key}'>";
-                            echo "<label>Value: </label>";
-                            echo "<input data-key='value' onkeyup='validateUnserializedFields(event)' value='{$value}'>";
-                            echo "<button onclick='removeItem(event)'>Remove</button>";
-                            echo "</section>";
-                        }
-                        echo "</div>";
-                        // add new button
-                        echo
-                        "<div>
-                            <button onclick='addItemField(event)'>Add new</button>
-                        </div>";
-                    } else {
-                        echo "<input value='{$current}'>";
-                        echo "<br/>";
+                echo "<label for='filename'>File name</label>";
+                echo "<input name='filename' value='{$_FILES['file']['name']}'>";
+                echo "<button type='submit' value='save'>Save file</button>";
+
+
+                for ($iter = 0; !feof($file); ++$iter) {
+                    // input name will be record[iterator][keyname]
+                    $name = "record[{$iter}]";
+                    $record = fgetcsv($file);
+                    // empty line
+                    if (empty($record)) {
+                        break;
                     }
+                    echo "<div class='csv-data' data-id='{$record[$id_idx]}'>";
+                    for ($i = 0; $i < count($record); ++$i) {
+                        $recordName = "{$name}[{$fields[$i]}]";
+                        $current = $record[$i];
+                        echo "<strong>$fields[$i]: </strong>";
+                        if (preg_match($serialized_pattern, $current)) {
+                            $data = unserialize($current);
+                            echo "<div class='unserialized' data-serialize='$fields[$i]'>";
+                            $j = 0;
+                            foreach ($data as $key => $value) {
+                                echo "<section>";
+                                echo "<label>Key: </label>";
+                                echo "<input name='{$recordName}[{$j}][key]' data-key='key' onkeyup='validateUnserializedFields(event)' value='{$key}'>";
+                                echo "<label>Value: </label>";
+                                echo "<input name='{$recordName}[{$j}][value]' data-key='value' onkeyup='validateUnserializedFields(event)' value='{$value}'>";
+                                echo "<button onclick='removeItem(event)'>Remove</button>";
+                                echo "</section>";
+                                ++$j;
+                            }
+                            echo "</div>";
+                            // add new button
+                            echo
+                            "<div>
+                                <button onclick='addItemField(event)'>Add new</button>
+                            </div>";
+                        } else {
+                            echo "<input name='{$recordName}' value='{$current}'>";
+                            echo "<br/>";
+                        }
+                    }
+                    echo "</div>";
                 }
-                echo "</div>";
+                fclose($file);
+            } catch (Exception $e) {
+                echo "<div class='error'>{$e->getMessage()}</div>";
             }
-            fclose($file);
-        } catch (Exception $e) {
-            echo "<div class='error'>{$e->getMessage()}</div>";
         }
-    }
-?>
+    ?>
+</form>
 
 <script>
     const resetLoadFileButton = () => loadFileButton.disabled = !file.value;
---
+++ savefile.php
@@ -0,0 +1,7 @@
+<?php
+    if($_POST) {
+        echo '<pre>';
+        var_dump($_POST);
+        echo '</pre>';
+    }
+?>
